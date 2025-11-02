25c200643c85cb7becfae6c39051840684fbf697
Olisemeka Nmarkwe <134287241+OliseNS@users.noreply.github.com>
2025-11-02 17:59:31 -0600

    Debug is false so that it is suitable for prod

--- app.py
+++ app.py
@@ -554,4 +554,4 @@ def error():
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5001))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
